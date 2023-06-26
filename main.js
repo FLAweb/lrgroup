0f35f4ca91981225048d48498eef2faa7e2f081f
FLAweb <facundo_allegrini@hotmail.com>
2023-06-26 20:02:40 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+window.sr = ScrollReveal();
+
+    sr.reveal('.titulo', {
+        duration: 3000,
+        origin: 'right',
+        distance: '-200px'
+    });
+
+    sr.reveal('header', {
+        duration: 1000,
+        origin: 'bottom',
+        distance: '-10px'
+    });
+
+    sr.reveal('.section2', {
+        duration: 3000,
+        origin: 'bottom',
+        distance: '-10px'
+    });
+
+    sr.reveal('.article-2', {
+        duration: 300,
+        origin: 'top',
+        distance: '-100px'
+    });
